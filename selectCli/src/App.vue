43c1cff8078672814983700c5dfaa38ff5737885
Serhat Serter <serhatserter@yandex.com>
2019-07-15 14:07:53 +0300

    new cli test

---
+++ selectCli/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+        <div id="maindiv">
+        
+        <div v-for="(blocks,index) in posts" :key="index">
+            <box-block 
+                v-bind:boxhead = "blocks.boxhead"
+                v-bind:boxintro = "blocks.boxintro" 
+                v-bind:boxcontent = "blocks.boxcontent"
+            ></box-block>
+            <br>
+        </div>
+          
+    </div>
+  </div>
+</template>
+
+<script>
+import BoxBlock from './components/BoxBlock.vue'
+export default {
+  name: 'app',
+  components: { BoxBlock },
+
+  data () {
+    return {
+      posts: [],
+    }
+  },
+  
+  created(){
+    fetch('src/datas/dataBlog.json')
+        .then((res) => {return res.json()})
+        .then((res) => {
+
+            this.posts = res;
+
+        })
+  }
+}
+</script>
+
+<style>
+body {
+    height: 100%;
+    margin: 0;
+    background-color: cornflowerblue;
+    
+}
+
+#maindiv{
+    display: block;
+    width: 60%;
+    background-color: whitesmoke;
+    padding: 20px;
+    
+}
+
+p{
+    color:black;
+    padding: 10px;
+
+}
+</style>
---
+++ selectCli/src/components/BoxBlock.vue
@@ -0,0 +1,60 @@
+<template>
+        <div class="boxback" @click="link(contentdisplay)">
+        <h3 class="boxHead">{{boxhead}}</h3>
+        <h4 class="boxIntro">{{boxintro}}</h4>
+        
+        <div class="content" v-bind:style = "{display: contentdisplay}">
+            <p>{{boxcontent}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                contentdisplay: "none",
+                posts: this.posts,
+            }
+        },
+        
+        methods:{
+            link(getstr){
+                if(this.contentdisplay=="none") this.contentdisplay="block";
+                else this.contentdisplay="none";
+            }
+        },
+        props:{
+            boxhead:{
+                type:String,
+                required: true,
+            },
+            boxintro:{
+                type:String,
+                required: true,
+            },
+            boxcontent:{
+                type:String,
+                required: true,
+        },
+    },
+    }
+
+</script>
+
+<style>
+.boxback{
+    background-color: lightgray;
+    text-align: center; 
+    padding: 20px;
+}
+.boxback:hover{
+    background-color: cornflowerblue;
+    color: whitesmoke;
+    
+}
+.content{
+    height: 100px;
+    background-color: whitesmoke;
+}
+</style>
