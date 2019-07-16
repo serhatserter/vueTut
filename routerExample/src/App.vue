3540ce95d519d5134b9379788c9e27318a2605dc
Serhat Serter <serhatserter@yandex.com>
2019-07-16 11:00:09 +0300

    router Example

---
+++ routerExample/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/foo">Foo</router-link>
+    <router-link to="/bar">Bar</router-link>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+
+}
+
+</style>
---
+++ routerExample/src/components/UserProfile.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>UserProfile</h1>
+
+</template>
+
+<script>
+    export default{
+        name: 'UserProfile'
+    }
+</script>
